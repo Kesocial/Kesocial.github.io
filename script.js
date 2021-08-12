dedceb3e85a4d0424619d7b159a481ee72939ee1
Acevedo Kevin <87038684+Kesocial@users.noreply.github.com>
2021-08-11 21:37:50 -0300

    TP 1 Terminado
    Falta el metodo que trabaja con this.genero

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-class CPersona {
-    constructor(nombre, edad, dni, genero, peso, altura) {
-        this.nombre = nombre;
-        this.edad = edad;
-        this.dni = dni;
-        this.genero = genero;
-        this.peso = peso;
-        this.altura = altura;
-    }
-
-    calcularIMC() {
-        let imc = this.peso / Math.pow(this.altura, 2);
-        let valor;
-        if (imc < 20) {
-            valor = -1;
-        }
-        if ((imc >= 20) && (imc <= 25)) {
-            valor = 0;
-        }
-        if (imc > 25) {
-            valor = 1;
-        }
-        return valor;
-    }
-}
-
-let enviar = document.getElementById("enviar");
 let btn1 = document.getElementById("btn");
 let btnTrabajos = document.getElementById("trabajosPracticos");
 let btnInicio = document.getElementById("inicio");
@@ -43,17 +16,4 @@ btnTrabajos.onclick = function() {
 }
 btnInicio.onclick = function() {
     navbar[0].classList.remove("active");
-}
-enviar.onclick = function() {
-    let nombre = document.getElementById("nombre");
-    let edad = document.getElementById("edad");
-    let dni = document.getElementById("dni");
-    let genero = document.getElementById("genero");
-    let altura = parseFloat(document.getElementById("altura"));
-    let peso = parseInt(document.getElementById("peso"));
-
-    const p1 = new CPersona(nombre, edad, dni, genero, peso, altura);
-    let imc = p1.calcularIMC();
-    document.write(altura);
-    document.write(peso);
 }
