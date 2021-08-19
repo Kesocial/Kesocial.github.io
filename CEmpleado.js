ef534435a1f4c6a8f16fba4434f69a8bf212e7cb
Acevedo Kevin <87038684+Kesocial@users.noreply.github.com>
2021-08-19 09:20:14 -0300

    Add files via upload

---
+++ CEmpleado.js
@@ -0,0 +1,14 @@
+class CEmpleado{
+
+constructor(nombre, apellido, Categoria,
+    antiguedad,
+    idioma,
+    hijosConGuarderia,
+    presentismo, KmPorMovilidadPropia){
+         
+}
+
+
+}
+
+e1 = new CEmpleado("pepito","ratimatu","")
